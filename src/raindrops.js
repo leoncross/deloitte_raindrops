6197802572c3ca5971bc6d61be422e8090828e51
leoncross <leon@lscross.co.uk>
2019-02-17 11:35:01 +0000

    refactored raindrops, cleaned up repo

--- src/raindrops.js
+++ src/raindrops.js
@@ -1,9 +1,9 @@
 function raindrops (n) {
   var string = ''
-  if (n % 3 === 0 ) string += 'Pling'
-  if (n % 5 === 0 ) string += 'Plang'
-  if (n % 7 === 0 ) string += 'Plong'
-  return (string.length === 0) ? n : string;
+  if (n % 3 === 0) string += 'Pling'
+  if (n % 5 === 0) string += 'Plang'
+  if (n % 7 === 0) string += 'Plong'
+  return (string.length === 0) ? n : string
 }
 
 module.exports = raindrops
