2dd0c71611c3fe94ebfdca39c6344e5c83b21323
leoncross <leon@lscross.co.uk>
2019-02-17 11:22:54 +0000

    returns pling if factor of 3

--- spec/raindropsSpec.js
+++ spec/raindropsSpec.js
@@ -2,6 +2,9 @@ var raindrops = require('../src/raindrops.js')
 
 describe('raindrops', function () {
   it('returns a number', function () {
-  expect(raindrops(5)).toEqual(5)
+    expect(raindrops(5)).toEqual(5)
+  })
+  it('returns pling if factor of 3', function () {
+    expect(raindrops(3)).toEqual('Pling')
   })
 })
--- src/raindrops.js
+++ src/raindrops.js
@@ -1,5 +1,7 @@
 function raindrops (n) {
-  return n
+  var string = ''
+  if (n % 3 === 0 ) string += 'Pling'
+  return (string.length === 0) ? n : string;
 }
 
 module.exports = raindrops
